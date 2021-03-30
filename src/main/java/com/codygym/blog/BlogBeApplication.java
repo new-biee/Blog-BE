37d325b54132077eb59afa8936a6f5187aa82d69
new-biee <anhvtth1907004@fpt.edu.vn>
2021-03-31 01:26:49 +0700

    kick off

---
+++ src/main/java/com/codygym/blog/BlogBeApplication.java
@@ -0,0 +1,13 @@
+package com.codygym.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBeApplication.class, args);
+    }
+
+}
